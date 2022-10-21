5d235a2257cde16282797dca85b557b566c939d0
Rich Burroughs <rb@loft.sh>
2022-10-20 19:46:25 -0700

    add create deployment

---
+++ kubecon-2022-NA/vcluster/create-deployment.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+########################
+# include the magic
+########################
+. /usr/local/bin/demo-magic.sh
+
+########################
+# Configure the options
+########################
+
+#
+# speed at which to simulate typing. bigger num = faster
+#
+TYPE_SPEED=17
+
+#
+# custom prompt
+#
+# see http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/bash-prompt-escape-sequences.html for escape sequences
+#
+# DEMO_PROMPT="${GREEN}➜ ${CYAN}\W $ "
+
+# text color
+# DEMO_CMD_COLOR=$BLACK
+
+# Prompt timeout is 1 second. Each wait will be that length.
+# PROMPT_TIMEOUT=1
+
+# hide the evidence
+clear
+
+# put your demo awesomeness here
+
+pe "vcluster connect kubecon"
+
+pe "kubectl get namespaces"
+
+pe "kubectl create namespace nginx"
+
+pe "kubectl create deployment nginx --image=nginx --replicas=4 -n nginx"
+
+pe "kubectl get pods -n nginx"
+
+pe "vcluster disconnect"
+
+pe "kubectl get namespaces"
+
+pe "kubectl get pods -n vcluster-kubecon"
+
+# show a prompt so as not to reveal our true nature after
+# the demo has concluded
+p ""
+
+
+
